2f8d31d0d7c1dfd239fcd769a2e100504366d7d3
kerrongordon <kgpsounds.com@gmail.com>
2015-06-07 13:15:11 -0400

    update widget for font page

--- plugins/kgp-strose-widgets/strose-gallery/home-gallery.php
+++ plugins/kgp-strose-widgets/strose-gallery/home-gallery.php
@@ -13,10 +13,10 @@ function kgp_strose_gallery_plugin() {
 
    if ($galleryPosts->have_posts()) : while ($galleryPosts->have_posts()) : $galleryPosts->the_post(); ?>
 		
-		<div class="m-all t-1of4 d-1of4 t-c cf">
-   			<?php //the_title(); ?>
+		<div class="m-all t-1of2 d-1of4 t-c cf">
    			<span class="img-round h-img-round">
    				<?php the_post_thumbnail( 'bones-thumb-200' ); ?>
+               <span class="round-test"><a href="<?php the_permalink(); ?>"><h1 class="h2 entry-title"><?php echo substr(the_title('', '', FALSE), 0, 15); ?>...</h1></a></span>
    			</span>
    		</div>
 
@@ -25,6 +25,5 @@ function kgp_strose_gallery_plugin() {
    		else :
    			 echo '<div class="no-widgets"><h2>Add some photos to Gallery</h2></div>';
    	endif;
-
 
 } ?>
